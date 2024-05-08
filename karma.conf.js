2c7040adc9ab659217b905bd2309949da3904788
sebastianhsrd123 <hugo2171910@correo.uis.edu.co>
2024-05-08 04:13:28 -0500

    Cambio de nombre

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/maria-colombia-aprende'),
+      dir: require('path').join(__dirname, './coverage/maria-blog'),
       subdir: '.',
       reporters: [
         { type: 'html' },
