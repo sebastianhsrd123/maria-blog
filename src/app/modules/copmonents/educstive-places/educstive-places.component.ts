7844c2551cf1826bcdf3f59d92efb6d60e60b550
sebastianhsrd123 <hugo2171910@correo.uis.edu.co>
2024-05-16 06:56:21 -0500

    Cambios solicitados al blog- corrección ortografica

--- src/app/modules/copmonents/educstive-places/educstive-places.component.ts
+++ src/app/modules/copmonents/educstive-places/educstive-places.component.ts
@@ -22,12 +22,12 @@ export class EducstivePlacesComponent implements OnInit {
   constructor() {
     this.header = {code: "Código", department: "Departamento", province: "Municipio", DANE: "DANE", institution: "Institución", name: "Nombre Sede"};
     this.content = [
-      {code: "NPC013", department: "CASANARE", province: "TAURAMENA", DANE: "185410000016", institution: "INSTITUCION EDUCATIVA JOSE MARIA CORDOBA", name: "JOSE MARIA CORDOBA"},
-      {code: "NPC014", department: "CASANARE", province: "VILLANUEVA", DANE: "185440000053", institution: "INSTITUCION EDUCATIVA FABIO RIVEROS", name: "FABIO RIVEROS"},
-      {code: "NPC015", department: "CASANARE", province: "VILLANUEVA", DANE: "185440000118", institution: "INSTITUCION EDUCATIVA EZEQUIEL MORENO Y DIAZ", name: "EZEQUIEL MORENO Y DIAZ"},
-      {code: "NPC016", department: "META", province: "VILLAVICENCIO", DANE: "150001001860", institution: "COLEGIO DEPARTAMENTAL LA ESPERANZA", name: "COLEGIO DPTAL LA ESPERANZA"},
-      {code: "NPC017", department: "META", province: "VILLAVICENCIO", DANE: "150001007833", institution: "COLEGIO SILVIA APONTE RODRIGUEZ", name: "COLEGIO SILVIA APONTE RODRIGUEZ"},
-      {code: "NPC018", department: "META", province: "RESTREPO", DANE: "150606000172", institution: "INSTITUCION EDUCATIVA EMILIANDO RESTREPO ECHAVARRIA", name: "SEDE PRINCIPAL EMILIANDO RESTREPO ECHAVARRIA"}
+      {code: "NPC-013", department: "CASANARE", province: "TAURAMENA", DANE: "185410000016", institution: "INSTITUCION EDUCATIVA JOSÉ MARÍA CÓRDOBA", name: "JOSÉ MARÍA CÓRDOBA"},
+      {code: "NPC-014", department: "CASANARE", province: "VILLANUEVA", DANE: "185440000053", institution: "INSTITUCION EDUCATIVA FABIO RIVEROS", name: "FABIO RIVEROS"},
+      {code: "NPC-015", department: "CASANARE", province: "VILLANUEVA", DANE: "185440000118", institution: "INSTITUCION EDUCATIVA EZEQUIEL MORENO Y DÍAZ", name: "EZEQUIEL MORENO Y DÍAZ"},
+      {code: "NPC-016", department: "META", province: "VILLAVICENCIO", DANE: "150001001860", institution: "COLEGIO DEPARTAMENTAL LA ESPERANZA", name: "COLEGIO DPTAL LA ESPERANZA"},
+      {code: "NPC-017", department: "META", province: "VILLAVICENCIO", DANE: "150001007833", institution: "COLEGIO SILVIA APONTE RODRÍGUEZ", name: "COLEGIO SILVIA APONTE RODRÍGUEZ"},
+      {code: "NPC-018", department: "META", province: "RESTREPO", DANE: "150606000172", institution: "INSTITUCION EDUCATIVA EMILIANO RESTREPO ECHAVARRÍA", name: "SEDE PRINCIPAL EMILIANO RESTREPO ECHAVARRÍA"}
     ];
    }
 
